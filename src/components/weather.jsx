c5e5c522d90a51a3c038ad82e103c6764cb2e77d
Dharitap9803 <dharitapatel.098@gmail.com>
2025-07-02 18:58:17 +0530

    fifth commit

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -10,7 +10,7 @@ import windyicon from  '../assets/windy.png'
 import pressureicon from '../assets/pressure.jpg'
 import React, { useState } from 'react'
 import feelsLike from '../assets/feelsLike.jpg'
-import minTemp from '../assets/mintemp.jpg' //changed 
+import minTemp from '../assets/mintemp.jpg' //changed the file 
 import maxTemp from '../assets/maxTemp.jpg'
 import rain from '../assets/rain.jpg'
 import sunrise from '../assets/sunrise.jpg'
